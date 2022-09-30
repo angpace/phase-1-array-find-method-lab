2a1176ac014aa4fca953b508ba85a6caa42ecb83
Angela Pace <angpace13@gmail.com>
2022-09-30 13:29:10 -0400

    commit tree 96cac708dd1dfaafd52adf45b4b8869b431e2c92

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+]
+
+function superbowlWin(record) {
+  debugger
+  let flop = record.find((arr) => arr.result === "W")
+  console.log(flop)
+  if (flop){
+    console.log(flop.year)
+    return flop.year}
+  //else {
+   // return "undefined"
+ // }
+ }
+// @return [String] year, or undefined
+
+	// try to find the game object where there was a win
+		// .find()??
+		// return the year
+	// if not, ...
+		// return undefined
+    //ecord.find(superbowlWin)
